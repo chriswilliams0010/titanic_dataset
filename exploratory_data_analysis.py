c62f374d22326515cde56d262cf2d94fe0b0a8a0
Chris Williams <chris.williams0010@gmail.com>
2021-07-11 17:18:11 -0400

    extracted titles from names

---
+++ exploratory_data_analysis.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+path = r'train.csv'
+
+data = pd.read_csv(path)
+reserve_data = data.copy()
+tot_rec = len(data)
+missing = data.info(verbose=True)
+
+# extract title from name and place it in a new column
+data['Title'] = [i[1].split()[0] for i in (j.split(',') for j in (x for x in data['Name']))]
+
+# fill the missing values in 'Age'
+# bin the ages
+
+# get the family count and place it in a new column
+
+# bin the fare values
+
+# split the first letter off the 'Cabin'
+
+# fill the two missing values in 'Embarked'
+.idea/inspectionProfiles/
+	.idea/misc.xml
+	.idea/modules.xml
+	.idea/titanic_dataset.iml
+	.idea/vcs.xml
+	titanic_env/
