64d3a22ce4ae183e7c8f12560d0fc3dcf526e50a
Chris Williams <chris.williams0010@gmail.com>
2021-07-11 22:28:28 -0400

    label encoding title and sex

--- exploratory_data_analysis.py
+++ exploratory_data_analysis.py
@@ -1,16 +1,29 @@
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 path = r'train.csv'
 
 data = pd.read_csv(path)
 reserve_data = data.copy()
 tot_rec = len(data)
-missing = data.info(verbose=True)
 
 # extract title from name and place it in a new column
 data['Title'] = [i[1].split()[0] for i in (j.split(',') for j in (x for x in data['Name']))]
+le = LabelEncoder()
+data['Title'] = le.fit_transform(data['Title'])
+
+# convert 'Sex' to labels
+le2 = LabelEncoder()
+data['Sex'] = le2.fit_transform(data['Sex'])
 
 # fill the missing values in 'Age'
+cr = data.corr()
+missing_age = data[data['Age'].isnull()]
+for i in range(data['Title'].max()):
+	x = round(data['Age'][data['Title'] == i].mean(), 2)
+	print(i, ":", x)
+
+
 # bin the ages
 
 # get the family count and place it in a new column
