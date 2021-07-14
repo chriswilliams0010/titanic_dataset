5bc6285f944831593fc831d2ba4087de04a423ce
Chris Williams <chris.williams0010@gmail.com>
2021-07-13 22:12:10 -0400

    bin fare

--- exploratory_data_analysis.py
+++ exploratory_data_analysis.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
-import re
 
 path = r'train.csv'
 
@@ -24,17 +23,24 @@ data['Age'] = data['Age'].fillna(data.groupby('Title')['Age'].transform('mean'))
 # bin the ages
 min_age = data['Age'].min()
 max_age = data['Age'].max()
-bins = [0., 18., 35., 60., 80.]
-labels = [0, 1, 2, 3]
-data['Age'] = pd.cut(data['Age'], bins=bins, labels=labels)
+bins_age= [0., 18., 35., 60., 80.]
+labels_age = [0, 1, 2, 3]
+data['Age'] = pd.cut(data['Age'], bins=bins_age, labels=labels_age)
 
 # get the family count and place it in a new column
 data['Family'] = data['SibSp'] + data['Parch'] + 1
 data = data.drop(['SibSp', 'Parch'], axis=1)
 
-# bin the fare values
+# drop 'Ticket'
+data = data.drop(['Ticket'], axis=1)
 
-# split the first letter off the 'Cabin'
-# data['cabin_letter'] = [re.split('[0-9]+', i, flags=re.IGNORECASE)[0] for i in data['Cabin']]
+# bin the fare values
+min_fare = data['Fare'].min()
+max_fare = data['Fare'].max()
+bins_fare = [0., 50., 200., 600.]
+labels_fare = [0, 1, 2]
+data['Fare'] = pd.cut(data['Fare'], bins=bins_fare, labels=labels_fare)
+
+# drop'Cabin'
 
 # fill the two missing values in 'Embarked'
