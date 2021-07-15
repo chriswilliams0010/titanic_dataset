f62b7b355ae8bcb4b30d5621f2ed7308b25021e5
Chris Williams <chris.williams0010@gmail.com>
2021-07-15 07:03:36 -0400

    balanced forest and easy ensemble

---
+++ titanic_ee.py
@@ -0,0 +1,22 @@
+from exploratory_data_analysis import data
+import numpy as np
+from sklearn.model_selection import train_test_split
+from imblearn.ensemble import EasyEnsembleClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+
+y = np.array(data['Survived'])
+X = np.array(data.iloc[:, 2:])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y)
+
+clf = EasyEnsembleClassifier()
+
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+
+print('\nModel accuracy score: ', accuracy_score(y_test, y_pred))
+print('\nConfusion Matrix\n', confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
